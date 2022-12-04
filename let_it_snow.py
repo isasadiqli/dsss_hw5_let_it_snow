9c2447e5ab496987a38bb9a3dc13417233cf9b5d
isasadiqli <62072356+isasadiqli@users.noreply.github.com>
2022-12-04 21:56:11 +0100

    random colors for each snowflake is implemented in let_it_snow.py file

--- let_it_snow.py
+++ let_it_snow.py
@@ -14,6 +14,7 @@ def main(speed=0, bg_color="grey"):
   
  
     """TODO: define different colors here"""
+    colors = ['red', 'green', 'blue', 'violet', 'black', 'white', 'yellow', 'brown']
 
 
     for _ in range(10):
@@ -22,6 +23,7 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
+        myTurtle.color(colors[np.random.randint(0, 8)])
 
         # Go to the start position of the snowflake
         myTurtle.penup()
